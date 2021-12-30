997617564c414255e0afb4b96be2f7b1222fb7e6
yaman0007 <92988324+yaman0007@users.noreply.github.com>
2021-12-30 16:26:08 +0530

    Initial commit

---
+++ src/components/products.js
@@ -0,0 +1,83 @@
+export const products = [
+  {
+    id: 1,
+    title: "Citroen C5 Aircross",
+    description: "black in color",
+    price: "32.80 Lakh",
+    img:
+      "https://media.zigcdn.com/media/model/2021/Feb/side-view-476095516_930x620.jpg",
+    quantity: 1
+  },
+  {
+    id: 2,
+    title: "Mahindra XUV700",
+    description: "blue in color",
+    price: "22.99 Lakh",
+    img:
+      "https://media.zigcdn.com/media/model/2021/Sep/front-1-4-left-2036444044_930x620.jpg",
+    quantity: 1
+  },
+  {
+    id: 3,
+    title: "Maruti Suzuki Vitara Brezza",
+    description: "red in color",
+    price: "7.61 - 11.19 Lakh",
+    img:
+      "https://media.zigcdn.com/media/model/2020/Feb/front-1-4-left-190291530_930x620.jpg",
+    quantity: 1
+  },
+  {
+    id: 4,
+    title: "Maruti Suzuki Swift",
+    description: "Red in color",
+    price: "5.85 - 8.67 Lakh",
+    img:
+      "https://media.zigcdn.com/media/model/2021/Feb/front-1-4-left-635868423_930x620.jpg",
+    quantity: 1
+  },
+  {
+    id: 5,
+    title: "Mahindra Scorpio",
+    description: "white in color",
+    price: "12.77 - 17.61 Lakh",
+    img:
+      "https://media.zigcdn.com/media/model/2018/Oct/front-1-4-left-1164069781_930x620.jpg",
+    quantity: 1
+  },
+  {
+    id: 6,
+    title: "Tata Punch",
+    description: "blue in color",
+    price: "5.49 - 9.09 Lakh",
+    img:
+      "https://media.zigcdn.com/media/model/2021/Oct/rear-3-4-left-1769305311_930x620.jpg",
+    quantity: 1
+  },
+  {
+    id: 7,
+    title: "Mahindra Thar",
+    description: "red in color",
+    price: "12.78 - 15.08 Lakh",
+    img:
+      "https://media.zigcdn.com/media/model/2020/Aug/side-view-759471168_930x620.jpg",
+    quantity: 1
+  },
+  {
+    id: 8,
+    title: "Toyota Fortuner",
+    description: "Black in color",
+    price: "30.73 - 42.33 Lakh",
+    img:
+      "https://media.zigcdn.com/media/model/2021/Jan/front-1-4-left-2038483973_930x620.jpg",
+    quantity: 1
+  },
+  {
+    id: 9,
+    title: "Kia Seltos",
+    description: "red in color",
+    price: "18.10 Lakh ",
+    img:
+      "https://media.zigcdn.com/media/model/2021/May/front-1-4-left-1901906406_930x620.jpg",
+    quantity: 1
+  }
+];
